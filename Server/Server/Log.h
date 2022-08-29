671108b9c81eb0ff1a8cbffe49daee723454ba72
Reoul <dydqja1358@gmail.com>
2022-08-29 15:45:46 +0900

    로그 변경
    로그 변경

--- Server/Server/Log.h
+++ Server/Server/Log.h
@@ -7,15 +7,15 @@
 #include <spdlog/sinks/rotating_file_sink.h>
 
 #define log_assert(expression) (void)(																						\
-            (!!(expression)) || _assertion_log_write(#expression, __FILE__, (unsigned)(__LINE__)) ||	\
+            (!!(expression)) || _assertion_log_error_write(#expression, __FILE__, (unsigned)(__LINE__)) ||	\
             (_wassert(_CRT_WIDE(#expression), _CRT_WIDE(__FILE__), (unsigned)(__LINE__)), 0)								\
         )
 
 inline void LogInit()
 {
-	auto consoleLogger = spdlog::stdout_color_mt("console");
-	auto fileLogger = spdlog::rotating_logger_mt("log", "ServerLogs/log.txt", 3 * 1024 * 1024, 100);
-	auto testLogger = spdlog::rotating_logger_mt("test", "ServerLogs/testLog.txt", 3 * 1024 * 1024, 100);
+	shared_ptr<spdlog::logger> consoleLogger = spdlog::stdout_color_mt("console");
+	shared_ptr<spdlog::logger> fileLogger = spdlog::rotating_logger_mt("log", "ServerLogs/log.txt", 3 * 1024 * 1024, 100);
+	shared_ptr<spdlog::logger> testLogger = spdlog::rotating_logger_mt("test", "ServerLogs/testLog.txt", 3 * 1024 * 1024, 100);
 }
 
 template<typename ... Arguments>
@@ -38,7 +38,7 @@ template<typename ... Arguments>
 inline void LogWriteWarning(const char* format, Arguments... args)
 {
 	const auto logger = spdlog::get("log");
-	logger->error(format, args...);
+	logger->warn(format, args...);
 	logger->flush();
 }
 
@@ -50,7 +50,7 @@ inline void LogWriteError(const char* format, Arguments... args)
 	logger->flush();
 }
 
-inline bool _assertion_log_write(const char* message, const char* file, unsigned int line)
+inline bool _assertion_log_error_write(const char* message, const char* file, unsigned int line)
 {
 	LogWriteError("Assertion failed: {0}, {1}, line {2}", message, file, line);
 	return false;
